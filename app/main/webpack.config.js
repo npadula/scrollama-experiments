1c397d8b836239f309e4a933f4ce36536230f394
Nicolas <nicolasvpadula@gmail.com>
2018-08-09 21:23:19 -0300

    Got Scrollama working. Header and some assets.

--- app/main/webpack.config.js
+++ app/main/webpack.config.js
@@ -19,6 +19,15 @@ module.exports = {
           }
         ]
       },
+	   {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}
+          }
+        ]
+      },
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, "css-loader"]
