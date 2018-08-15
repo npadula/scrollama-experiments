ab046b2bb68e15b74ef4aa7c0eff8e0be10e844b
Nicolas <nicolasvpadula@gmail.com>
2018-08-14 22:49:43 -0300

    Fixes some issues with appropiate resizing of step cards.

--- app/main/src/index.js
+++ app/main/src/index.js
@@ -15,13 +15,13 @@ window.stickyfill = stickyfill;
 
 var typography = new Typography(funstonTheme);
 
- WebFont.load({
+WebFont.load({
     google: {
-      //families: ['Droid Sans', 'Droid Serif']
-	  families: ['Patua One', 'Cabin Condensed']
-	  //families: ['Source Sans Pro', 'Bitter']
+        //families: ['Droid Sans', 'Droid Serif']
+        families: ['Patua One', 'Cabin Condensed']
+            //families: ['Source Sans Pro', 'Bitter']
     }
-  });
+});
 
 
 // Or insert styles directly into the <head> (works well for client-only
@@ -29,96 +29,120 @@ var typography = new Typography(funstonTheme);
 typography.injectStyles();
 
 // using d3 for convenience
-		const container = d3.select('#scroll');
-		const graphic = container.select('.scroll__graphic');
-		const chart = graphic.select('.chart');
-		const text = container.select('.scroll__text');
-		const step = text.selectAll('.step');
+const container = d3.select('#scroll');
+const graphic = container.select('.scroll__graphic');
+const chart = graphic.select('.chart');
+const text = container.select('.scroll__text');
+const step = text.selectAll('.step');
 
-		// initialize the scrollama
-		const scroller = scrollama();
+// initialize the scrollama
+const scroller = scrollama();
 
-		// generic window resize listener event
-		function handleResize() {
-			// 1. update height of step elements
-			var stepHeight = Math.floor(window.innerHeight * 0.75);
-			step.style('height', stepHeight + 'px');
-
-			// 2. update width/height of graphic element
-			var bodyWidth = d3.select('body').node().offsetWidth;
-			var textWidth = text.node().offsetWidth;
-
-			var graphicWidth = bodyWidth - textWidth;
-
-			graphic
-				.style('width', graphicWidth + 'px')
-				.style('height', window.innerHeight + 'px');
-
-			var chartMargin = 32;
-			var chartWidth = graphic.node().offsetWidth - chartMargin;
-
-			chart
-				.style('width', chartWidth + 'px')
-				.style('height', Math.floor(window.innerHeight / 2) + 'px');
+// generic window resize listener event
+function handleResize() {
+    console.log("handleResize");
+    // 1. update height of step elements
+    const baseStepHeight = Math.floor(window.innerHeight * 0.7);
+    //step.style('height', baseStepHeight + 'px');
 
 
-			// 3. tell scrollama to update new element dimensions
-			scroller.resize();
-		}
+    for (let s of step.nodes()) {
+        const node = d3.select(s).select(".post-content").node();
+        const innerHeight = node ? node.offsetHeight + 100 : baseStepHeight;
 
-		// scrollama event handlers
-		function handleStepEnter(response) {
-			// response = { element, direction, index }
+        d3.select(s).style("height", innerHeight + 'px');
 
-			// add color to current step only
-			step.classed('is-active', function (d, i) {
-				return i === response.index;
-			})
+    }
 
-			// update graphic based on step
-			chart.select('p').text(response.index + 1)
-		}
+    // 2. update width/height of graphic element
+    const bodyWidth = d3.select('body').node().offsetWidth;
+    const textWidth = text.node().offsetWidth;
 
-		function handleContainerEnter(response) {
-			// response = { direction }
-		}
+    var graphicWidth = bodyWidth - textWidth;
 
-		function handleContainerExit(response) {
-			// response = { direction }
-		}
+    graphic
+        .style('width', graphicWidth + 'px')
+        .style('height', window.innerHeight + 'px');
 
-		function setupStickyfill() {
-			d3.selectAll('.sticky').each(function () {
-				Stickyfill.add(this);
-			});
-		}
+    const chartMargin = 32;
+    const chartWidth = graphic.node().offsetWidth - chartMargin;
 
-		function init() {
-			setupStickyfill();
+    chart
+        .style('width', chartWidth + 'px')
+        .style('height', Math.floor(window.innerHeight / 2) + 'px');
 
-			// 1. force a resize on load to ensure proper dimensions are sent to scrollama
-			handleResize();
 
-			// 2. setup the scroller passing options
-			// this will also initialize trigger observations
-			// 3. bind scrollama event handlers (this can be chained like below)
-			scroller.setup({
-				container: '#scroll',
-				graphic: '.scroll__graphic',
-				text: '.scroll__text',
-				step: '.scroll__text .step',
-				debug: true,//Comment to disable "step trigger" dashed line
-			})
-				.onStepEnter(handleStepEnter)
-				.onContainerEnter(handleContainerEnter)
-				.onContainerExit(handleContainerExit);
+    // 3. tell scrollama to update new element dimensions
+    scroller.resize();
+}
 
-			// setup resize event
-			window.addEventListener('resize', handleResize);
-		}
+// scrollama event handlers
+function handleStepEnter(response) {
+    // response = { element, direction, index }
 
-		// kick things off
-		init();
-		
-		
-		
+    // add color to current step only
+    step.classed('is-active', function(d, i) {
+        return i === response.index;
+    });
+
+    // update graphic based on step
+    chart.select('p').text(response.index + 1);
+}
+
+function handleContainerEnter(response) {
+    // response = { direction }
+}
+
+function handleContainerExit(response) {
+    // response = { direction }
+}
+
+function setupStickyfill() {
+    d3.selectAll('.sticky').each(function() {
+        Stickyfill.add(this);
+    });
+}
+
+function init() {
+    console.log("init");
+    setupStickyfill();
+    // 1. force a resize on load to ensure proper dimensions are sent to scrollama
+    handleResize();
+
+
+    // 2. setup the scroller passing options
+    // this will also initialize trigger observations
+    // 3. bind scrollama event handlers (this can be chained like below)
+    scroller.setup({
+            container: '#scroll',
+            graphic: '.scroll__graphic',
+            text: '.scroll__text',
+            step: '.scroll__text .step',
+            debug: true, //Comment to disable "step trigger" dashed line
+        })
+        .onStepEnter(handleStepEnter)
+        .onContainerEnter(handleContainerEnter)
+        .onContainerExit(handleContainerExit);
+
+    window.addEventListener('resize', handleResize);
+
+}
+
+
+
+// setup resize event
+
+window.addEventListener('load', () => {
+    console.log("load");
+
+    init();
+
+
+    setTimeout(() => {
+        handleResize();
+    }, 500);
+
+
+});
+
+// kick things off
